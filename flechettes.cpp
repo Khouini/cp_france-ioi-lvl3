6a80d4e70d5f737096e41bf8e21e6f1b25732ec7
Yacine Khouini <khouini.medyacine@gmail.com>
2023-07-04 00:24:46 +0100

    Test 1	Success Fléchettes

--- flechettes.cpp
+++ flechettes.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    // cout << "Entrez le nombre de lettres: ";
-    int n = 5;
-    // cin >> n;
+    cout << "Entrez le nombre de lettres: ";
+    int n;
+    cin >> n;
 
     if (n < 1 || n > 26)
     {
@@ -14,7 +14,11 @@ int main()
     bool weBack = false;
     int loopSize = n * 2 - 1;
     int startInterval = 0, endInterval = loopSize - 1;
-    char myArray[9] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+    char myArray[loopSize];
+    for (int i = 0; i < loopSize; i++)
+    {
+        myArray[i] = 'a';
+    }
     int updateValue = 1;
     for (int i = 0; i < loopSize; i++)
     {
@@ -24,41 +28,34 @@ int main()
             bool beforeLastColumn = j < loopSize - 1;
             bool allowedUpdate = j > startInterval && j < endInterval;
             bool lastColum = j == loopSize - 1;
-            bool mediumAttempt = (i == (loopSize) / 2);
+            bool mediumAttempt = (i == (loopSize) / 2) && lastColum && !isFirstLine;
             if (!isFirstLine && beforeLastColumn && allowedUpdate)
             {
                 myArray[j] = myArray[j] + updateValue;
             }
             cout << myArray[j];
-            if (lastColum)
+            if (lastColum && !isFirstLine)
             {
-                if (!isFirstLine)
+                if (mediumAttempt)
                 {
-                    if (mediumAttempt)
+                    weBack = true;
+                    updateValue = -1;
+                }
+                else
+                {
+                    if (weBack)
                     {
-                        // startInterval = j;
-                        // endInterval = j;
-                        weBack = true;
-                        updateValue = -1;
+                        startInterval--;
+                        endInterval++;
                     }
                     else
                     {
-                        if (weBack)
-                        {
-                            startInterval--;
-                            endInterval++;
-                        }
-                        else
-                        {
-                            startInterval++;
-                            endInterval--;
-                        }
+                        startInterval++;
+                        endInterval--;
                     }
                 }
             }
         }
         cout << endl;
     }
-
-    return 0;
 }
